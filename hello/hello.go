99e06302c97c27bdc8f86596a036f594eafa574c
超 张 <imagine243@gmail.com>
2018-02-12 18:27:16 +0800

    add code

--- hello/hello.go
+++ hello/hello.go
@@ -1,9 +1,38 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type TTT struct{}
 
 func main() {
-	fmt.Println("hello")
+	// fmt.Println("hello world")
+	// fmt.Println("go " + "lang")
+	// fmt.Println("1+1 = ", 1+1)
+	// fmt.Println("7.0 / 3.0 = ", 7.0/3.0)
+	// fmt.Println(true && false)
+	// fmt.Println(true || false)
+	// fmt.Println(!true)
+
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i++
+	}
+
+	for j := 5; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	for n := 0; n <= 12; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+	ntl
 }
