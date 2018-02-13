c7ab547b455747adfb31cacd330f7ad92def0586
chao <imagine243@gmail.com>
2018-02-13 20:31:03 +0800

    practice

--- hello/hello.go
+++ hello/hello.go
@@ -1,8 +1,5 @@
 package main
 
-import "fmt"
-import "time"
-
 type TTT struct{}
 
 func main() {
@@ -36,44 +33,63 @@ func main() {
 	//     fmt.Println(n)
 	// }
 
-	i := 2
-	fmt.Print("write ", i, " as ")
-	switch i {
-	case 1:
-		fmt.Println("one")
-	case 2:
-		fmt.Println("two")
-	case 3:
-		fmt.Println("three")
-	}
+	// i := 2
+	// fmt.Print("write ", i, " as ")
+	// switch i {
+	// case 1:
+	//     fmt.Println("one")
+	// case 2:
+	//     fmt.Println("two")
+	// case 3:
+	//     fmt.Println("three")
+	// }
+	//
+	// switch time.Now().Weekday() {
+	// case time.Saturday, time.Sunday:
+	//     fmt.Println("this is the weekend")
+	// default:
+	//     fmt.Println("it is a weekday")
+	// }
+	//
+	// t := time.Now()
+	// switch {
+	// case t.Hour() < 12:
+	//     fmt.Println("it's before noon")
+	// default:
+	//     fmt.Println("it's afert noon")
+	// }
+	//
+	// whoami := func(i interface{}) {
+	//     switch t := i.(type) {
+	//     case bool:
+	//         fmt.Println("i am a bool")
+	//     case int:
+	//         fmt.Println("i am a int")
+	//     default:
+	//         fmt.Printf("don't konw type %T", t)
+	//     }
+	// }
+	//
+	// whoami(true)
+	// whoami(5)
+	// whoami("hey")
 
-	switch time.Now().Weekday() {
-	case time.Saturday, time.Sunday:
-		fmt.Println("this is the weekend")
-	default:
-		fmt.Println("it is a weekday")
-	}
-
-	t := time.Now()
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("it's before noon")
-	default:
-		fmt.Println("it's afert noon")
-	}
-
-	whoami := func(i interface{}) {
-		switch t := i.(type) {
-		case bool:
-			fmt.Println("i am a bool")
-		case int:
-			fmt.Println("i am a int")
-		default:
-			fmt.Printf("don't konw type %T", t)
-		}
-	}
-
-	whoami(true)
-	whoami(5)
-	whoami("hey")
+	// var a [5]int
+	// fmt.Println("emp: ", a)
+	//
+	// a[4] = 100
+	// fmt.Println("set : ", a)
+	// fmt.Println("get : ", a[4])
+	// fmt.Println("len : ", len(a))
+	//
+	// b := [5]int{1, 2, 3, 4, 5}
+	// fmt.Println("dcl : ", b)
+	//
+	// var twoD [2][3]int
+	// for i := 0; i < 2; i++ {
+	//     for j := 0; j < 3; j++ {
+	//         twoD[i][j] = i + j
+	//     }
+	// }
+	// fmt.Println("2d : ", twoD)
 }
