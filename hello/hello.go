b8e5cdeed7b0850f952f921c29aa32a48c2ef1c9
chao <imagine243@gmail.com>
2018-02-13 20:05:39 +0800

    add test

--- hello/hello.go
+++ hello/hello.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "time"
 
 type TTT struct{}
 
@@ -13,26 +14,66 @@ func main() {
 	// fmt.Println(true || false)
 	// fmt.Println(!true)
 
-	i := 1
-	for i <= 3 {
-		fmt.Println(i)
-		i++
+	// i := 1
+	// for i <= 3 {
+	//     fmt.Println(i)
+	//     i++
+	// }
+	//
+	// for j := 5; j <= 9; j++ {
+	//     fmt.Println(j)
+	// }
+	//
+	// for {
+	//     fmt.Println("loop")
+	//     break
+	// }
+	//
+	// for n := 0; n <= 12; n++ {
+	//     if n%2 == 0 {
+	//         continue
+	//     }
+	//     fmt.Println(n)
+	// }
+
+	i := 2
+	fmt.Print("write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
 	}
 
-	for j := 5; j <= 9; j++ {
-		fmt.Println(j)
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("this is the weekend")
+	default:
+		fmt.Println("it is a weekday")
 	}
 
-	for {
-		fmt.Println("loop")
-		break
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("it's before noon")
+	default:
+		fmt.Println("it's afert noon")
 	}
 
-	for n := 0; n <= 12; n++ {
-		if n%2 == 0 {
-			continue
+	whoami := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("i am a bool")
+		case int:
+			fmt.Println("i am a int")
+		default:
+			fmt.Printf("don't konw type %T", t)
 		}
-		fmt.Println(n)
 	}
-	ntl
+
+	whoami(true)
+	whoami(5)
+	whoami("hey")
 }
