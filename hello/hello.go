5fd9937763b6592a0ee580d98c48402467ec4574
chao <imagine243@gmail.com>
2018-02-15 17:50:01 +0800

    prectice

--- hello/hello.go
+++ hello/hello.go
@@ -234,6 +234,26 @@ func main() {
 	// sp.age = 51
 	// fmt.Println(s.age)
 
+	r := rect{width: 10, height: 20}
+
+	fmt.Println("area : ", r.area())
+	fmt.Println("perim : ", r.perim())
+
+	rp := &r
+	fmt.Println("area : ", rp.area())
+	fmt.Println("perim : ", rp.perim())
+}
+
+type rect struct {
+	width, height int
+}
+
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+func (r rect) perim() int {
+	return 2*r.width + 2*r.height
 }
 
 func zeroval(ival int) {
