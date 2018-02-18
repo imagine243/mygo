c0871de9a9bedd5fded96783b8908f3c6360d934
chao <imagine243@gmail.com>
2018-02-18 18:03:45 +0800

    practice

--- hello1/main.go
+++ hello1/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type geometry interface {
 	area() float64
@@ -15,6 +18,31 @@ type circle struct {
 	radius float64
 }
 
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+
+func (r rect) perim() float64 {
+	return 2*r.width + 2*r.height
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
 func main() {
 	fmt.Println("hello world")
+	r := rect{3, 4}
+	c := circle{5}
+	measure(r)
+	measure(c)
 }
