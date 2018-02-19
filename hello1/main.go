51a920eb550ca648eddaceb8ea5250c1d2bdd321
chao <imagine243@gmail.com>
2018-02-19 09:52:41 +0800

    practice

--- hello1/main.go
+++ hello1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -39,10 +40,57 @@ func measure(g geometry) {
 	fmt.Println(g.area())
 	fmt.Println(g.perim())
 }
+
+func f1(arg int) (int, error) {
+	if arg == 42 {
+		return -1, errors.New("can't work with 42")
+	}
+	return arg + 3, nil
+}
+
+type argError struct {
+	arg  int
+	prob string
+}
+
+func (e *argError) Error() string {
+	return fmt.Sprintf("%d - %s", e.arg, e.prob)
+}
+
+func f2(arg int) (int, error) {
+	if arg == 42 {
+		return -1, &argError{arg, "can't work with it"}
+	}
+
+	return arg + 3, nil
+}
+
 func main() {
 	fmt.Println("hello world")
 	r := rect{3, 4}
 	c := circle{5}
 	measure(r)
 	measure(c)
+
+	for _, i := range []int{7, 42} {
+		if r, e := f1(i); e != nil {
+			fmt.Println("f1 failed : ", e)
+		} else {
+			fmt.Println("f1 worked : ", r)
+		}
+	}
+
+	for _, i := range []int{7, 42} {
+		if r, e := f2(i); e != nil {
+			fmt.Println("f2 failed : ", e)
+		} else {
+			fmt.Println("f2 worked : ", r)
+		}
+	}
+
+	_, e := f2(42)
+	if ae, ok := e.(*argError); ok {
+		fmt.Println(ae.arg)
+		fmt.Println(ae.prob)
+	}
 }
