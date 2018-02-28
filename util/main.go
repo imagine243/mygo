a24a655e30cac64ca22c1a90db3d4d6ec0a665df
imagine243 <imagine243@gmail.com>
2018-02-28 20:28:25 +0800

    practice

--- util/main.go
+++ util/main.go
@@ -2,36 +2,94 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-	fmt.Println("hello util")
-	fmt.Print(rand.Intn(100), ",")
-	fmt.Print(rand.Intn(100))
+	// fmt.Println("hello util")
+	// fmt.Print(rand.Intn(100), ",")
+	// fmt.Print(rand.Intn(100))
+	//
+	// fmt.Println()
+	//
+	// fmt.Println(rand.Float64())
+	//
+	// fmt.Print(rand.Float64()*5+5, ",")
+	// fmt.Print(rand.Float64()*5 + 5)
+	// fmt.Println()
+	//
+	// s1 := rand.NewSource(time.Now().UnixNano())
+	// r1 := rand.New(s1)
+	//
+	// fmt.Print(r1.Intn(100), ",")
+	// fmt.Println(r1.Intn(100))
+	//
+	// s2 := rand.NewSource(42)
+	// r2 := rand.New(s2)
+	// fmt.Print(r2.Intn(100), ",")
+	// fmt.Println(r2.Intn(100))
+	//
+	// s3 := rand.NewSource(42)
+	// r3 := rand.New(s3)
+	// fmt.Print(r3.Intn(100), ",")
+	// fmt.Println(r3.Intn(100))
 
-	fmt.Println()
+	// dat, err := ioutil.ReadFile("/tmp/dat")
+	// check(err)
+	// fmt.Print(string(dat))
 
-	fmt.Println(rand.Float64())
+	// dateCmd := exec.Command("date")
+	// dateOut, err := dateCmd.Output()
+	// check(err)
+	// fmt.Println(">date")
+	// fmt.Println(string(dateOut))
+	//
+	// grepCmd := exec.Command("grep", "hello")
+	// grepIn, _ := grepCmd.StdinPipe()
+	// grepOut, _ := grepCmd.StdoutPipe()
+	// grepCmd.Start()
+	// grepIn.Write([]byte("hello grep \n goodbye grep"))
+	// grepIn.Close()
+	// grepBytes, _ := ioutil.ReadAll(grepOut)
+	// grepCmd.Wait()
+	// fmt.Println(string(grepBytes))
+	//
+	// lsCmd := exec.Command("bash", "-c", "ls -a -l -h")
+	// lsOut, err := lsCmd.Output()
+	// check(err)
+	// fmt.Println(">ls -a -l -h")
+	// fmt.Println(string(lsOut))
 
-	fmt.Print(rand.Float64()*5+5, ",")
-	fmt.Print(rand.Float64()*5 + 5)
-	fmt.Println()
+	// binary, lookErr := exec.LookPath("ls")
+	// check(lookErr)
+	//
+	// args := []string{"ls", "-a", "-l", "-h"}
+	// env := os.Environ()
+	//
+	// execErr := syscall.Exec(binary, args, env)
+	// check(execErr)
 
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
 
-	fmt.Print(r1.Intn(100), ",")
-	fmt.Println(r1.Intn(100))
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	s2 := rand.NewSource(42)
-	r2 := rand.New(s2)
-	fmt.Print(r2.Intn(100), ",")
-	fmt.Println(r2.Intn(100))
+	go func() {
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
 
-	s3 := rand.NewSource(42)
-	r3 := rand.New(s3)
-	fmt.Print(r3.Intn(100), ",")
-	fmt.Println(r3.Intn(100))
+	fmt.Println("awaiting signal")
+	<-done
+	fmt.Println("exiting")
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
